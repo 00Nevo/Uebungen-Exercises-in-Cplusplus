fc60ba8668acb56047fdac7b672e093247919610
NEVO <william.mcintosh@zoho.com>
2025-03-27 19:42:00 +0100

    Create ch1.cpp

---
+++ ch1.cpp
@@ -0,0 +1,35 @@
+
+Book: 
+C++ for dummies all in one.
+
+Chapter 1: Configuring
+Your System
+
+Obtaining a Copy of C++ 14
+
+Obtaining Code::Blocks
+
+Installing Code::Blocks
+
+Working with Windows
+
+Touring the Essential Code::Blocks Features
+
+Starting Code::Blocks for the first time
+
+Opening the sample projects
+
+Viewing the essential windows
+
+Using the Start Here window
+
+Using the Management window
+
+Using the Logs & Others window
+
+Selecting a compiler
+
+Chapter 2: Creating Your
+First C++ Application
+
+  
