04c313eb15cebf81ba4ad5881e91e6ac8bae4baf
NEVO <albertoborgato9@gmail.com>
2025-04-08 21:17:43 +0200

    Create Ch2.cpp

---
+++ Ch2.cpp
@@ -0,0 +1,18 @@
+Chapter 2: Creating Your
+First C++ Application
+
+Code::Blocks Creating a Project
+Understanding projects
+Defining your first project
+Building and executing your first application
+Typing the Code
+Starting with Main
+Showing Information
+Doing some math
+ Ordering the operations
+ Going overboard
+ Pairing the parentheses
+Tabbing your output
+Let Your Application Run Away
+
+Chapter 3: Storing Data in C++
