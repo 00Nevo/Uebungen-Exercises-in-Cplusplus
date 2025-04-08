1f34fc1d81618bb12fa0e65b08ac5c937bb98079
NEVO <albertoborgato9@gmail.com>
2025-04-08 21:48:12 +0200

    Update Ch2.cpp

--- Ch2.cpp
+++ Ch2.cpp
@@ -16,3 +16,32 @@ Tabbing your output
 Let Your Application Run Away
 
 Chapter 3: Storing Data in C++
+
+//make the r version of the following code 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int mynumber;
+mynumber = 10;
+cout << mynumber << endl;
+return 0;
+}
+
+ChatGPT:
+To write an equivalent version of the provided C++ code in R, we can make use of basic R syntax. Here's how it would look:
+R Version:
+
+# Assign value to variable
+mynumber <- 10
+
+# Print the value to the console
+print(mynumber)
+
+Explanation:
+    <- is used in R for assignment (equivalent to = in other languages like C++).
+    print() is used in R to output values to the console, similar to cout in C++.
+
+Output:
+This will display 10 on the R console, just like the C++ program.
