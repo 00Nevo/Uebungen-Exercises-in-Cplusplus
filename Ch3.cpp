bab0e7a341d1cab985126ac4784c620b22f280a2
NEVO <albertoborgato9@gmail.com>
2025-04-11 22:44:55 +0200

    Create Ch3.cpp

---
+++ Ch3.cpp
@@ -0,0 +1,35 @@
+Chapter 3: Storing Data in C++
+
+Putting Your Data Places: Variables
+Creating an integer variable
+Listing 3-1: Creating a Variable
+Declaring multiple variables
+Changing values
+Listing 3-2: Changing a Variable
+Setting one variable equal to another
+Initializing a variable
+Creating a great name for yourself
+Manipulating Integer Variables
+Adding integer variables
+Multiplying integer variables
+Dividing integer variables
+Listing 3-5: Finding Quotients and Remainders
+Characters
+Null character
+Nonprintable and other cool characters
+Strings
+Listing 3-6: Using Brackets to Access Individual Characters in a
+String
+Getting a part of a string
+Listing 3-7: Using the string Type to Create a String Variable
+Changing part of a string
+Adding onto a string
+Adding two strings
+Deciding between Conditional Operators
+Listing 3-8: Using the Conditional Operator to Do Comparisons
+Telling the Truth with Boolean Variables
+Reading from the Console
+Listing 3-9: Using the Conditional Operator to Make Comparisons
+
+Chapter 4: Directing the
+Application Flow
